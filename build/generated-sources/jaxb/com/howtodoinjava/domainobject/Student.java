50e6971b071ac68734b416dad45d83fd69b33bdc
xcj1235 <hechanggi1235@yahoo.co.jp>
2019-03-01 01:01:32 +0900

    修正2

--- build/generated-sources/jaxb/com/howtodoinjava/domainobject/Student.java
+++ build/generated-sources/jaxb/com/howtodoinjava/domainobject/Student.java
@@ -2,7 +2,7 @@
 // このファイルは、JavaTM Architecture for XML Binding(JAXB) Reference Implementation、v2.2.11によって生成されました 
 // <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>を参照してください 
 // ソース・スキーマの再コンパイル時にこのファイルの変更は失われます。 
-// 生成日: 2019.02.19 時間 12:41:43 AM JST 
+// 生成日: 2019.02.28 時間 11:39:45 PM JST 
 //
 
 
